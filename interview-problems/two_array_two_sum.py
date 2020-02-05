f01c302537eefea7d4ed2a5e809fe51cd366ebc8
Jake Shams <jshams62@gmail.com>
2020-02-04 23:57:10 -0800

    implemented the second solution

--- interview-problems/two_array_two_sum.py
+++ interview-problems/two_array_two_sum.py
@@ -1,3 +1,26 @@
+def nearest_binary_search(sorted_arr, t, start=0, end=None):
+    '''return the index of the closest number to target t in
+    sorted ar. Uses binary search'''
+    if end is None:
+        end = len(sorted_arr) - 1
+    if start == end:
+        if sorted_arr[start] > t and start != 0:
+            if abs(sorted_arr[start - 1] - t) < abs(sorted_arr[start] - t):
+                return start - 1
+        elif sorted_arr[start] < t and start != len(sorted_arr) - 1:
+            if abs(sorted_arr[start + 1] - t) < abs(sorted_arr[start] - t):
+                return start + 1
+        return start
+    middle = (start + end) // 2
+    if sorted_arr[middle] == t:
+        return middle
+    elif sorted_arr[middle] > t:
+        end = middle - 1
+    else:
+        start = middle + 1
+    return nearest_binary_search(sorted_arr, t, start, end)
+
+
 def two_array_two_sum1(nums1, nums2, target):
     best = None
     best_diff = float('inf')
@@ -11,7 +34,21 @@ def two_array_two_sum1(nums1, nums2, target):
 
 
 def two_array_two_sum2(nums1, nums2, target):
-    return None
+    '''sorts the larger array and iterate over the smaller array
+    and find the closest value to its complement'''
+    shorter_arr = nums1 if len(nums1) < len(nums2) else nums2
+    sorted_arr = sorted(nums1) if len(nums1) >= len(nums2) else sorted(nums2)
+    best_pair = None
+    best_diff = float('inf')
+    for num in shorter_arr:
+        complement = target - num
+        print(sorted_arr, complement)
+        comp_index = nearest_binary_search(sorted_arr, complement)
+        diff = abs(target - (num + sorted_arr[comp_index]))
+        if diff < best_diff:
+            best_pair = (num, sorted_arr[comp_index])
+            best_diff = diff
+    return best_pair
 
 
 def two_array_two_sum3(nums1, nums2, target):
