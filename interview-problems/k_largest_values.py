5fa25ea8594bec4c1907b10c6e066d1aa3e46bdc
Jake Shams <jshams62@gmail.com>
2020-02-05 01:14:07 -0800

    added docstrings with descriptions and time complexitites

--- interview-problems/k_largest_values.py
+++ interview-problems/k_largest_values.py
@@ -2,11 +2,16 @@ from heapq import heapify, heappop, heappush
 
 
 def k_largest_values1(nums, k):
-    '''reverse sort k and return a slice k first items'''
+    '''reverse sort nums and return a slice of the k first items
+    Time complexity: O(nlogn)'''
     return sorted(nums, reverse=True)[:k]
 
 
 def k_largest_values2(nums, k):
+    '''find the largest value, add it to an array of top values, then
+    replace its value with negative infinity. Find the next greatest
+    value k times and add them to an array then return the array
+    Time complexity: O(k*n)'''
     nums = nums[::]
     k_top_vals = []
     for _ in range(k):
@@ -22,12 +27,21 @@ def k_largest_values2(nums, k):
 
 
 def k_largest_values3(nums, k):
+    '''heapify nums then remove the largest k items in the heap.
+    Negates all numbers because this built in heap is a minheap and this
+    requires a max heap
+    Time complexity: O(n) + O(klongn) -> O(n)'''
     max_heap = [-num for num in nums]
     heapify(max_heap)
     return [-heappop(max_heap) for _ in range(k)]
 
 
 def k_largest_values4(nums, k):
+    '''iterates over nums and uses a minheap of size k to store the k largest
+    values seen so far. if a number is greater than the min item in the
+    min heap, remove the min and add the num. after iterating over the array
+    pop from the minheap to get the k largest values.
+    Time complexity: O(nlogn)'''
     min_heap = []
     for num in nums:
         if len(min_heap) < k:
