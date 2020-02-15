757055cbea61f7724aae0754f4aa15f2b972a2fe
Jake Shams <jshams62@gmail.com>
2020-02-15 03:07:56 -0800

    added comments

--- binary-tree-problems/reverse_tree.py
+++ binary-tree-problems/reverse_tree.py
@@ -5,9 +5,12 @@ def reverse_tree(tree, node=None):
     '''reverse the nodes of a binary tree'''
     if node is None:
         node = tree.root
+    # swap the left and right nodes
     node.left, node.right = node.right, node.left
+    # if the node has a left child, recurse on it
     if node.left is not None:
         reverse_tree(tree, node.left)
+    # if the node has a right child, recurse on it
     if node.right is not None:
         reverse_tree(tree, node.right)
 
