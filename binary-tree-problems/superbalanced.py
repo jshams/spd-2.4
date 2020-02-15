bb66553d6b003f428a207772e623bb611551c5bd
Jake Shams <jshams62@gmail.com>
2020-02-15 03:07:05 -0800

    correction

--- binary-tree-problems/superbalanced.py
+++ binary-tree-problems/superbalanced.py
@@ -1,70 +1,68 @@
 from binary_tree import BinaryTree
+from queue import Queue
 
 
-def is_superbalanced(tree, node=None):
-    '''a tree is considered superbalanced if the difference in height of
-    any two children of the same parent are less than or eq to 1.
-    This function eturns a boolean indicating whether the tree is
-    superbalanced, a boolean is only returned on the first iteration
-    or when the tree is not superbalanced, False. Otherwise the height
-    of the current subtree is returned to its parent.
-    '''
+def is_superbalanced_dfs(tree, node=None, parent_depth=0, depths=set()):
+    '''Let’s say a binary tree is superbalanced if the difference between
+     the depths of any two leaf nodes is at most one. Write a function
+    to check if a binary tree is superbalanced.'''
     if node is None:
         node = tree.root
-        # check if the tree is empty
         if node is None:
-            # if so it is balanced
             return True
-    # check if node has a left child
-    if node.left is not None:
-        # if so calculate its height, or determine if left is unbalanced
-        left_height = is_superbalanced(tree, node.left)
-        # if the left child returned false
-        if left_height is False:
-            # the tree was declared unbalanced, return false
+    if node.is_leaf():
+        depths.add(parent_depth + 1)
+        if len(depths) > 2:
             return False
-    # if there is no left child
+        return
     else:
-        # its height is -1
-        left_height = -1
-    # check if the node has a right child
-    if node.right is not None:
-        # if so calculate its height, or determine if right is unbalanced
-        right_height = is_superbalanced(tree, node.right)
-        # if right height is a false boolean
-        if right_height is False:
-            # the tree was declared unbalanced, return False
-            return False
-    # if there is no right child
-    else:
-        # right height is -1
-        right_height = -1
-    # if the difference between the left and right subtrees' heights > 1
-    if abs(right_height - left_height) > 1:
-        # the tree is not superbalanced
-        return False
-    # the height of the current node is 1 + the height of its taller child
-    if right_height > left_height:
-        height = right_height + 1
-    else:
-        height = left_height + 1
-    # if the node is the root (first iteration)
+        if node.left is not None:
+            is_superbalanced_dfs(tree, node.left, parent_depth + 1, depths)
+        if node.right is not None:
+            is_superbalanced_dfs(tree, node.right, parent_depth + 1, depths)
     if node == tree.root:
-        # the tree has passed all requirements of being superbalanced
+        if len(depths) > 3:
+            return False
+        elif len(depths) == 1:
+            return True
+        else:
+            d1, d2 = tuple(depths)
+            return abs(d1 - d2) < 2
+
+
+def is_superbalanced_bfs(tree):
+    '''Let’s say a binary tree is superbalanced if the difference between
+     the depths of any two leaf nodes is at most one. Write a function
+    to check if a binary tree is superbalanced.'''
+    node = tree.root
+    if node is None:
         return True
-    # the current subtree is bal, and its height is given to its parent
-    return height
-
-
-def test_is_superbalanced():
-    t = BinaryTree([1, 2, 4, 5, 3])
-    assert is_superbalanced(t) is False
-    t = BinaryTree([5, 3, 7, 1, 4, 6, 9, ])
-    assert is_superbalanced(t) is True
-    print('Is superbalanced passes the tests.')
-    t = BinaryTree()
-    assert is_superbalanced(t) is True
+    q = Queue()
+    q.put((node, 0))
+    min_leaf_depth = None
+    while not q.empty():
+        node, node_depth = q.get()
+        if min_leaf_depth is not None and node_depth - min_leaf_depth > 1:
+            return False
+        if node.is_leaf():
+            if min_leaf_depth is None:
+                min_leaf_depth = node_depth
+            else:
+                if node_depth - min_leaf_depth > 1:
+                    return False
+        if node.left is not None:
+            q.put((node.left, node_depth + 1))
+        if node.right is not None:
+            q.put((node.right, node_depth + 1))
+    return True
 
 
 if __name__ == '__main__':
-    test_is_superbalanced()
+    is_superbalanced = is_superbalanced_bfs
+    t = BinaryTree([1, 2, 3, 4, 5])
+    assert is_superbalanced(t) is True
+    t = BinaryTree([6, 5, 4, 3, 2, 7, 8])
+    assert is_superbalanced(t) is False
+    t = BinaryTree()
+    assert is_superbalanced(t) is True
+    print('Is superbalanced passes some tests.')
