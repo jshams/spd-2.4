9a5d04e78aa051ff448f70f98448eb293e02581d
Jake Shams <jshams62@gmail.com>
2020-02-15 03:09:48 -0800

    added iter and some more tests

--- binary-tree-problems/binary_tree.py
+++ binary-tree-problems/binary_tree.py
@@ -5,6 +5,9 @@ class BinaryTree():
         if len(items) > 0:
             self.add_items(items)
 
+    def __iter__(self):
+        yield from self.in_order_traversal()
+
     def add_items(self, items):
         for item in items:
             self.add_item(item)
@@ -58,20 +61,25 @@ def test_binary_tree():
     t.add_item(6)
     t.add_item(3)
     t.add_item(7)
+    # test root
     assert t.root.data == 6
     assert t.root.left.data == 3
     assert t.root.right.data == 7
+    # test items in order
     assert t.items_in_order() == [3, 6, 7]
     t = BinaryTree([3, 1, 5, 5])
     assert t.root.data == 3
+    # test size when adding duplicates
+    assert t.size == 3
     assert t.root.left.data == 1
     assert t.root.right.data == 5
     assert t.root.right.right is None
     assert t.root.right.left is None
     assert t.items_in_order() == [1, 3, 5]
-    return True
+    # test iter
+    assert [n.data for n in t] == [1, 3, 5]
 
 
 if __name__ == '__main__':
-    if test_binary_tree():
-        print('Tree passes tests')
+    test_binary_tree()
+    print('Tree passes tests')
