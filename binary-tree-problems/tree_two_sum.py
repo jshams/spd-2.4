3d2c344e57069fd9a43289e9e8bdb87fe7900eeb
Jake Shams <jshams62@gmail.com>
2020-02-15 03:07:34 -0800

    added comments

--- binary-tree-problems/tree_two_sum.py
+++ binary-tree-problems/tree_two_sum.py
@@ -2,19 +2,30 @@ from binary_tree import BinaryTree
 
 
 def tree_two_sum(bst, t):
-    '''given a binary search tree, and and a target integer t, return two numbers
-    within the tree that sum to t, or None if no pair exists'''
+    '''given a binary search tree of integers, and and a target integer t,
+    return two numbers within the tree that sum to t, or None if no pair exists
+    uses an in order and a reverse order generator on the trees to get one
+    small number and one large number and move inwards based on their sum to
+    find the two sum quickly.
+    Time complexity: O(n)
+    '''
+    # create a generator to generate items in order
     small_num_gen = in_order_tree_generator(bst)
+    # create a generator to generate items in reverse order
     large_num_gen = reverse_order_tree_generator(bst)
+    # grab the first numbers from each generator
     smaller_num = next(small_num_gen)
     larger_num = next(large_num_gen)
-    print(smaller_num, larger_num)
+    # continue checking for the sum till both generators reach the same node
     while smaller_num != larger_num:
+        # find the sum of the small and large nums
         curr_sum = smaller_num + larger_num
         if curr_sum == t:
             return smaller_num, larger_num
+        # if their sum is greater than t, find the next (smaller) large number
         elif curr_sum > t:
             larger_num = next(large_num_gen)
+        # if their sum is greater than t, find the next (larger) small number
         else:
             smaller_num = next(small_num_gen)
 
